7072a37305755f2ae708d29ccb018c892b530881
Patrick Pschorn <pardypaddy1@gmail.com>
2016-05-06 23:44:33 +0200

    added os detection

--- BusDriveApp/app/pages/home/home.ts
+++ BusDriveApp/app/pages/home/home.ts
@@ -24,6 +24,7 @@ export class HomePage {
         this.getbuslist();
         this.getlinelist();
         this.getstoplist();
+        this.getMobileOperatingSystem();
         //this.buslist = [{'title':"Bus 1"},{'title':'Bus 2'},{'title':'Bus 3'},{'title':'Bus 4'}];
         //this.linelist = [{'title':'Line 1'},{'title':'Line 2'},{'title':'Line 3'},{'title':'Line 4'}]
         //this.stoplist = [{'title':'Stop 1'},{'title':'Stop 2'},{'title':'Stop 3'},{'title':'Stop 4'},{'title':'Stop 5'}]
@@ -67,5 +68,25 @@ export class HomePage {
             linelist: this.foundlinelist,
             stoplist: this.foundstoplist
         });
+    }
+    getMobileOperatingSystem() {
+        var userAgent = navigator.userAgent || navigator.vendor;
+
+        if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
+        {
+            this.os =  'ios';
+
+        }
+        else if( userAgent.match( /Android/i ) )
+        {
+
+             this.os =  'Android';
+        }
+        else
+        {
+             this.os =  'unknown';
+        }   
+        
+        console.log("os detected: ",this.os);
     }
 }
