c0bfcc1e009c284cb8d6a1cb608667fe59187456
ttmher <mher@hotmail.de>
2016-05-06 02:38:34 +0200

    #7 #9 : map embedded, it also displays its own position
    ToDo: show waypoints ( stops ) and route on map.
    thanks to group2 for the idea how to solve our problem with "reference
    to google.maps.d.ts"

--- BusDriveApp/app/pages/map/map.ts
+++ BusDriveApp/app/pages/map/map.ts
@@ -1,9 +1,39 @@
-import {Page, Platform} from 'ionic-angular';
+import {Page} from 'ionic-angular';
+import {Geolocation} from 'ionic-native';
 
 @Page({
   templateUrl: 'build/pages/map/map.html',
 })
 export class MapPage {
+  private map;
+  private marker;
   constructor() {
+    this.loadMap()
+  }
+
+loadMap(){
+    
+    let options = {timeout: 10000, enableHighAccuracy: true};
+    
+    navigator.geolocation.getCurrentPosition((position) => {
+            
+            let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            let mapOptions = {
+                center: latLng,
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                zoomControl: true               
+            }
+            this.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+            this.marker = new google.maps.Marker({
+              position: latLng,
+              map: this.map,
+            }); 
+              
+           }, 
+           (error) => {
+             console.log(error);
+           }, options
+    );
   }
 }
--- BusDriveApp/typings/browser.d.ts
+++ BusDriveApp/typings/browser.d.ts
@@ -1,1 +1,2 @@
 /// <reference path="browser/ambient/es6-shim/index.d.ts" />
+/// <reference path="browser/ambient/google.maps/google.maps.d.ts" />
