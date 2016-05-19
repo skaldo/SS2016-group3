9113a1e9bd9181d8c0a75c39f4061cae955067e1
Oliver Säger <o_saeger13@cs.uni-kl.de>
2016-05-19 23:11:10 +0200

    #12 fixed typings

--- BusDriveApp/typings/browser.d.ts
+++ BusDriveApp/typings/browser.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="browser/ambient/es6-shim/index.d.ts" />
 /// <reference path="browser/ambient/google.maps/google.maps.d.ts" />
-/// <reference path="browser/jasmine/index.d.ts" />
-/// <reference path="browser/node/index.d.ts" />
+/// <reference path="browser/ambient/jasmine/index.d.ts" />
+/// <reference path="browser/ambient/node/index.d.ts" />
--- BusDriveApp/typings/main.d.ts
+++ BusDriveApp/typings/main.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="main/ambient/es6-shim/index.d.ts" />
 /// <reference path="main/ambient/google.maps/google.maps.d.ts" />
-/// <reference path="main/jasmine/index.d.ts" />
-/// <reference path="main/node/index.d.ts" />
+/// <reference path="main/ambient/jasmine/index.d.ts" />
+/// <reference path="main/ambient/node/index.d.ts" />
