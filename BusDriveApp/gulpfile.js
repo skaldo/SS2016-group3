35446968c527cb33473706f77645b496ffef0f1e
erikgruener <erikgruener@users.noreply.github.com>
2016-05-19 12:15:09 +0200

    hello99 gulpfileedit,travis

--- BusDriveApp/gulpfile.js
+++ BusDriveApp/gulpfile.js
@@ -3,7 +3,17 @@ var gulp = require('gulp'),
     del = require('del'),
     runSequence = require('run-sequence'),
     argv = process.argv;
+var Server = require('karma').Server;
 
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
 
 /**
  * Ionic hooks
