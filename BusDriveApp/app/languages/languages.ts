f013855b5c1c715afcdf7630c1a98774db65713a
Sascha Müller <s_mueller13@cs.uni-kl.de>
2016-05-23 18:27:01 +0200

    #26 Small GUI improvements
    server textfield moved to top, stopslist doesn't show stop id, tabs
    color changed to primary, new language variable in linelist

--- BusDriveApp/app/languages/languages.ts
+++ BusDriveApp/app/languages/languages.ts
@@ -7,7 +7,7 @@ export class Language {
     //passengers:string;
 
     constructor(public id:number, public name:string, public driveTitle:string, public passengers:string, public numberplate:string,
-    public chooseBus:string, public lineTitle:string, public mapTitle:string, public stopTitle:string,
+    public chooseBus:string, public lineTitle:string, public lineName:string, public mapTitle:string, public stopTitle:string,
     public alertTitle:string, public alertCancel:string){
         this.id = id;
         this.name = name;
@@ -20,15 +20,16 @@ export class Language {
         this.stopTitle = stopTitle;
         this.alertTitle = alertTitle;
         this.alertCancel = alertCancel;
+        this.lineName = lineName;
     }
     
 }
 
 
 export var en = new Language(0,"EN","Drive","Seats taken:","Numberplate:",
-    "Choose bus","Choose line","Map","Schedule","End tour?","Cancel");
+    "Choose bus","Choose line","Line","Map","Schedule","End tour?","Cancel");
 export var de = new Language(1,"DE","Fahren","Belegte Plätze:","Nummernschild",
-    "Wähle Bus aus","Wähle Linie aus","Karte","Fahrplan","Fahrt beenden?","Abbrechen");
+    "Wähle Bus aus","Wähle Linie aus","Linie","Karte","Fahrplan","Fahrt beenden?","Abbrechen");
 
 export var language;
 
