c0d3ee31064e3861b5cda167877bce58f69b22e1
ttmher <mher@hotmail.de>
2016-05-26 15:54:53 +0200

    #30 : transfered post command from TabsPage to Service, added new post command and changed interfaces

--- BusDriveApp/app/Services/lists.ts
+++ BusDriveApp/app/Services/lists.ts
@@ -2,33 +2,94 @@ import {Injectable} from 'angular2/core';
 import {Http, Headers} from 'angular2/http';
 
 /*
-  Created by Charel92
-  Edited by ttmher
-*/
+ Created by Charel92
+ Edited by ttmher
+ */
 
 @Injectable()
 export class Lists {
     constructor(private http:Http) {
     }
 
-// Hier wird die Methode deklariert die die Busse anfragt, später kommen noch ein paar mehr hinzu
+    /**
+     * @param serverURL URL of the server
+     * @returns JSON of busses
+     */
     getBusses(serverURL) {
-        let busses = this.http.get(serverURL+'/busses');
+        let busses = this.http.get(serverURL + "/busses");
         return busses;
     }
-
+    
+    /**
+     * @param serverURL URL of the Server
+     * @returns JSN of lines
+     */
     getLines(serverURL) {
-        let lines = this.http.get(serverURL+`/lines`);
+        let lines = this.http.get(serverURL + "/lines");
         return lines;
     }
 
+    /**
+     * @param serverURL URL of the Server
+     * @retruns JSON of stops
+     */
     getStops(serverURL) {
-        let stops = this.http.get(serverURL+`/stops`);
+        let stops = this.http.get(serverURL + "/stops");
         return stops;
     }
     
+    /**
+     * @param serverURL URL of the Server
+     * @retruns JSON of routes
+     */
     getRoutes(serverURL) {
-        let routes = this.http.get(serverURL+`/routes`);
+        let routes = this.http.get(serverURL + "/routes");
         return routes;
+    }
+
+    /**
+     * posts realTimeData to server
+     * @param serverURL URL of the Server
+     * @param busID ID of the selected bus
+     * @param longitude Longitude of current position
+     * @param latitude Latitude of current position
+     */
+    postRealTimeData(serverURL, busID, longitude, latitude) {
+        let realTimeData = JSON.stringify(
+            {
+                "busid": busID,
+                "position": {
+                    "type": "Point",
+                    "coordinatates": [longitude, latitude]
+                },
+                "timeStamp": Date.now()
+            })
+        let senddata = new XMLHttpRequest();
+        senddata.open('POST', serverURL + "/realTimeData");
+        senddata.setRequestHeader('Content-Type', 'application/json');
+        senddata.send(realTimeData)
+        console.log("Senden")
+        console.log("Latitude: ", latitude, "Longitude: ", longitude);
+        console.log("Bus: ", busID);
+    }
+
+    /**
+     * posts updateBusStatus to server
+     * @param serverURL URL of the Server
+     * @param busID ID of the selected bus
+     * @param lineID ID of the selected line
+     */
+    postBusStatus(serverURL, busID, lineID) {
+        let busStatus = JSON.stringify(
+            {
+                "lineid": lineID,
+                "busid": busID
+            })
+        let senddata = new XMLHttpRequest();
+        senddata.open('POST', serverURL + "/updateBusStatus");
+        senddata.setRequestHeader("Content-Type", "application/json");
+        senddata.send(busStatus)
+        console.log("Senden")
+        console.log("Bus: ", busID, "Line: ", lineID);
     }
 }
--- BusDriveApp/app/pages/tabs/tabs.ts
+++ BusDriveApp/app/pages/tabs/tabs.ts
@@ -8,9 +8,9 @@ import {Lists} from '../../Services/lists';
 import {language} from "../../languages/languages";
 
 /*
-  Created by ttmher
-  Edited by Charel92  
-*/
+ Created by ttmher
+ Edited by Charel92  and saskl
+ */
 
 @Page({
     templateUrl: 'build/pages/tabs/tabs.html',
@@ -20,21 +20,21 @@ import {language} from "../../languages/languages";
 export class TabsPage {
     private nav;
     private intervalID;
-    
+
     private stoplist = [];
     private linestops = [];
     private route = [];
     private lineroute = [];
     private rootParams = [];
-    
+
     private selectedbus;
     private selectedline;
     private serverURL;
-    
+
     private tab1Root;
     private tab2Root;
     private tab3Root;
-    
+
     public map;
     public drive;
     public stops;
@@ -44,33 +44,34 @@ export class TabsPage {
         this.tab1Root = DrivePage;
         this.tab2Root = MapPage;
         this.tab3Root = StopsPage;
-        
+
         this.selectedbus = navParams.get("selectedbus");
-        this.selectedline = navParams.get("selectedline");       
+        this.selectedline = navParams.get("selectedline");
         this.serverURL = navParams.get("URL");
-        
+
         this.getStoplist();
         this.getRoute();
         this.setRootParams();
-                
+
         this.map = language.mapTitle;
         this.drive = language.driveTitle;
         this.stops = language.stopTitle;
-                
-        this.intervalID = setInterval(this.sendCurrentStatus.bind(this), 15000)
+
+        this.intervalID = setInterval(this.sendrealTimeData.bind(this), 15000)
+        this.lists.postBusStatus(this.serverURL, this.selectedbus.id, this.selectedline.id)
+
     }
-    
+
     /**
-     * DE: Holt die Stoppliste vom Server und entfernt Stopps, die nicht zu der Line gehören 
-     * EN: gets the stoplist from the server and removes stops which do not belong to the line
-     */ 
+     * gets the stoplist from the server and removes stops which do not belong to the line
+     */
     getStoplist() {
         this.lists.getStops(this.serverURL).subscribe(
             data => {
                 this.stoplist = data.json();
-                for ( let index = 0; index < this.stoplist.length; index++) { 
-                    for ( let jndex = 0; jndex < this.stoplist[index].lines.length; jndex++){
-                        if (this.selectedline.id === this.stoplist[index].lines[jndex]){
+                for (let index = 0; index < this.stoplist.length; index++) {
+                    for (let jndex = 0; jndex < this.stoplist[index].lines.length; jndex++) {
+                        if (this.selectedline.id === this.stoplist[index].lines[jndex]) {
                             console.log("jetzt wird gepusht", this.stoplist[index].name)
                             this.linestops.push(this.stoplist[index]);
                         }
@@ -83,72 +84,50 @@ export class TabsPage {
     }
 
     /**
-     * DE: Holt die Routen vom Server und entfernt die Routen, die nicht zu der Line gehören 
-     * EN: gets the routes from the server and removes routes which do not belong to the line
-     */ 
-    getRoute(){
+     * gets the routes from the server and removes routes which do not belong to the line
+     */
+    getRoute() {
         this.lists.getRoutes(this.serverURL).subscribe(
             data => {
                 this.route = data.json();
-                console.log("jetzt wird die Route geladen:",this.selectedline.id);
-                for (var index = 0; index < this.route[this.selectedline.id-1].gpsData.length; index++) { 
+                console.log("jetzt wird die Route geladen:", this.selectedline.id);
+                for (var index = 0; index < this.route[this.selectedline.id - 1].gpsData.length; index++) {
                     this.lineroute.push({
-                        lat: this.route[this.selectedline.id-1].gpsData[index].latitude,
-                        lng: this.route[this.selectedline.id-1].gpsData[index].longitude                       
-                    })    
-                 } 
+                        lat: this.route[this.selectedline.id - 1].gpsData[index].latitude,
+                        lng: this.route[this.selectedline.id - 1].gpsData[index].longitude
+                    })
+                }
             },
             err => console.error(err),
             () => console.log('getRoute completed')
-        );               
+        );
     }
-    
+
     /**
-     * DE: Setzt die RootParams 
-     * EN: sets rootParams
+     * sets rootParams
      */
-    setRootParams(){
-         this.rootParams = [this.linestops, this.lineroute]
+    setRootParams() {
+        this.rootParams = [this.linestops, this.lineroute]
     }
-    
+
     /**
-     * DE: Sendet die aktuelle Position, die Id des gewählten Busses und der Line und die Zeit an den Server
-     * EN: sends the current position, the id of the selected bus and line and the time to the server
-     */ 
-    sendCurrentStatus() {
+     * sends the current position, the id of the selected bus and the time to the server
+     */
+    sendrealTimeData() {
         Geolocation.getCurrentPosition().then((resp) => {
             let latitude = resp.coords.latitude;
             let longitude = resp.coords.longitude;
-            let currentBusStatus = JSON.stringify(
-                {
-                "bus_id": this.selectedbus.id,
-                "line_id": this.selectedline.id,
-                "position": 
-                [
-                    {
-                    "longitude": longitude,
-                    "latitude": latitude
-                    }
-                ],
-                "timeStamp": Date.now()
-                })
-                let senddata = new XMLHttpRequest();
-                senddata.open('POST',this.serverURL+"/currentbusStatus");
-                senddata.setRequestHeader('Content-Type','application/json');
-                senddata.send(currentBusStatus)
-                console.log("Senden")
-                console.log("Latitude: ", latitude, "Longitude: ", longitude);
-                console.log("Bus: ", this.selectedbus.id, "Line: ", this.selectedline.id);
+            this.lists.postRealTimeData(this.serverURL, this.selectedbus.id, longitude, latitude)
         });
     }
-    
+
     /**
      * alert when leaving, if you click "OK" GUI will change to HomePage and you will stop sending, if you click "Abbrechen" nothing will happen.
      */
-    onPageWillLeave(){
+    onPageWillLeave() {
         let alert = Alert.create({
-        title: language.alertTitle,
-        buttons: [{
+            title: language.alertTitle,
+            buttons: [{
                 text: 'OK',
                 handler: () => {
                     console.log('alert confirmed');
@@ -156,14 +135,14 @@ export class TabsPage {
                     clearInterval(this.intervalID);
                 }
             },
-            {
-                text: language.alertCancel,
-                handler: () => {
-                    console.log('alert aborted');
-                }
-            }]
+                {
+                    text: language.alertCancel,
+                    handler: () => {
+                        console.log('alert aborted');
+                    }
+                }]
         });
-     this.nav.present(alert);   
+        this.nav.present(alert);
     }
 }
      
