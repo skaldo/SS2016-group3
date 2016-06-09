f38ccec9709bcac8dd53e2e43ec7f9f1f66c4d4f
Patrick <p_pschorn13@cs.uni-kl.de>
2016-06-09 12:31:38 +0200

    BusListSpec aktualisiert

--- BusDriveApp/test/structure/busListSpec.ts
+++ BusDriveApp/test/structure/busListSpec.ts
@@ -1,23 +1,38 @@
 import {BusListPage} from '../../app/pages/buslist/buslist.ts';
 import {Http, Response, ResponseOptions, Headers} from '@angular/http';
-import {Observable} from 'rxjs/observable';
-import {Lists} from '../../app/components/Services/lists.ts';
+import {Observable} from 'rxjs/RX';
 import {Assert} from '../util'; //imported from Steffens util according to his suggestion
+
+
+import {Page, NavController, NavParams, MenuController} from 'ionic-angular';
+import {LineListPage} from '../../app/pages/linelist/linelist.ts';
+import {BusDriveInterface} from '../../app/components/Services/busdriveinterface';
+import {Busses} from '../../app/components/Services/busses';
+import {SettingPage} from '../../app/components/setting/setting';
+
 /**
   Created by Oliver
   Edited by Erik
   BusListPage tests
 */
-let busListPage = BusListPage;
+
+
+  let nav:NavController;
+  let navParams:NavParams;
+  let settingPage:SettingPage;
+  let menuController:MenuController;
+
 
 describe('Dieser Test ist ein Beispiel',function(){
+  let busListPage:BusListPage;
   it('hat Zugriff auf andere files',function(){
     expect(typeof busListPage).toBe(typeof BusListPage);
   });
 });
 
-describe("setting up fake lists.ts for getbuslist test",function(){
-	let testData =   {busses:  [{
+describe("getting the Busses from the Server",function(){
+	
+  let testData =   {busses:  [{
       id: 1,
       numberPlate: "KL-AB345",
       color: "green",
@@ -29,19 +44,28 @@ describe("setting up fake lists.ts for getbuslist test",function(){
       color: "red",
       picture: "http://littlebabybum.com/wp-content/uploads/2015/01/wheels-on-the-bus-red.png"
     }]};
-	
-	let lists = <Lists>{
-		getBusses(serverUrl:string):Observable<Response>{
-			let response: Response = new Response(
+
+
+
+  //let busDriveInterface:BusDriveInterface;
+  //new BusDriveInterface(undefined:Http,mockedBuslist,undefined:Lines,undefined:Stops,undefined:Routes,undefined:Provider,undefined:CustomStops)
+  
+  //let testObject = new BusListPage(nav,navParams,busDriveInterface,menuController);
+  
+
+  let http = <Http> {
+    get(url:string):Observable<Response>{
+			let response:Response = new Response(
 				new ResponseOptions({body:testData})
 			);
 			return Observable.of(response);
-		}
-	};
-	let x,y,z;
-	let testObject = new BusListPage(x,y,lists,z);
-	it('testing get function', function(done){
-		Assert.equalJson(testData,testObject.getBuslist(), 'Wrong Buslist fetched');
-		done();
+    }
+  };
+
+ let bussesMock = new Busses(http);
+  
+	it('should load Bus entries to Busses.busses', function(done){
+		expect(bussesMock.getBusses()).not.toEqual([]);
+    done();
 	})
 })
