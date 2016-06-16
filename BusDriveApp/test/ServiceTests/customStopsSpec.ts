4be86a2866a38c07b6dcddf5e18ac3b92825676e
Oliver <s_oliver_s@web.de>
2016-06-16 11:29:32 +0200

    #51 added comments to customStopsSpec

--- BusDriveApp/test/ServiceTests/customStopsSpec.ts
+++ BusDriveApp/test/ServiceTests/customStopsSpec.ts
@@ -87,7 +87,7 @@ describe("the process of getting available line entries from the server",functio
         }
      };
     /**  
-        testing if the customstops variable empty is
+        testing if the customstops variable it not empty after requestCustomStops()
     */
     it('should load customstops entries', function(done){
         let customStopsMock:CustomStops = new CustomStops(http);  //	TypeError: undefined is not a constructor
@@ -96,7 +96,7 @@ describe("the process of getting available line entries from the server",functio
 
     });
     /**  
-        testing if the linecustomstops variable empty is
+        testing if the linecustomstops variable is not empty after getLineCustomStops()
     */
     it('should load customlinestops entries', function(done){
         let customStopsMock:CustomStops = new CustomStops(http);  //	TypeError: undefined is not a constructor
@@ -105,48 +105,72 @@ describe("the process of getting available line entries from the server",functio
 
     });
 
+    /**
+     * testing if getLineCustomStopsIds() does not return an empty list
+     */
     it('should load the ID of linecustomstops', function(){
       let customStopsMock:CustomStops = new CustomStops(http);
       customStopsMock.requestCustomStops("");
       expect(customStopsMock.getLineCustomStopsIds).not.toEqual([]);
     });
 
+    /**
+     * testing if getLineCustomStopsNames() does not return an empty list
+     */
     it('should load the names of linecustomstops', function(){
       let customStopsMock:CustomStops = new CustomStops(http);
       customStopsMock.requestCustomStops("");
       expect(customStopsMock.getLineCustomStopsNames).not.toEqual([]);
     });
 
+    /**
+     * testing if getLineCustomStopPickUpTimes() does not return an empty list
+     */
     it('should load the pickuptimes of linecustomstops', function(){
       let customStopsMock:CustomStops = new CustomStops(http);
       customStopsMock.requestCustomStops("");
       expect(customStopsMock.getLineCustomStopPickUpTimes()).not.toEqual([]);
     });
 
+    /**
+     * testing if getLineCustomStopsCoordinates() does not return an empty list
+     */
     it('should load the coordinates of linecustomstops', function(){
       let customStopsMock:CustomStops = new CustomStops(http);
       customStopsMock.requestCustomStops("");
       expect(customStopsMock.getLineCustomStopsCoordinates()).not.toEqual([]);
     });
 
+    /**
+     * testing if getLineCustomStopsNumberOfPersons() does not return an empty list
+     */
     it('should load the number of persons', function(){
       let customStopsMock:CustomStops = new CustomStops(http);
       customStopsMock.requestCustomStops("");
       expect(customStopsMock.getLineCustomStopsNumberOfPersons()).not.toEqual([]);
     });
 
+    /**
+     * testing if getLineCustomStopsAssistances() does not return an empty list
+     */
     it('should load the adresses of linecustomstops', function(){
       let customStopsMock:CustomStops = new CustomStops(http);
       customStopsMock.requestCustomStops("");
       expect(customStopsMock.getLineCustomStopsAddresses()).not.toEqual([]);
     });
 
+    /**
+     * testing if getLineCustomStopsAssistances() does not return an empty list
+     */
     it('should load the assistances of linecustomstops', function(){
       let customStopsMock:CustomStops = new CustomStops(http);
       customStopsMock.requestCustomStops("");
       expect(customStopsMock.getLineCustomStopsAssistances()).not.toEqual([]);
     });
 
+    /**
+     * testing if getLineCustomStopsAll() does not return an empty list
+     */
     it('should load the list of linecustomstops containing all information', function(){
       let customStopsMock:CustomStops = new CustomStops(http);
       customStopsMock.requestCustomStops("");
